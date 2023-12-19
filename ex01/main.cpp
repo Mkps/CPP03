83d44d889b78133c0b001ae48836b05dcb7f021e
Mkps <alex.loubiere@gmail.com>
2023-12-19 00:19:04 +0000

    some change to ClapTrap to reflect change made in ex00. Made the main more readable

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -15,14 +15,27 @@
 
 int	main(void)
 {
+    std::cout << "Constructors (should be different) : " << std::endl;
 	ClapTrap	norminou("Norminou");
+    std::cout << "Constructors (should be ClapTrap then ScavTrap) : " << std::endl;
 	ScavTrap	norminator("Norminator");
 
 	norminator.showStatus();
+    std::cout << "Attacks should be made as a Scavtrap : " << std::endl;
 	norminou.attack("the Sun");
 	norminator.attack("the Sun");
+    std::cout << "guardGate being exclusive to ScavTrap should obviously be made as a Scavtrap : " << std::endl;
 	norminator.guardGate();
 	norminator.guardGate();
+    std::cout << "These member functions are inherited from ClapTrap as such they should be done as ClapTrap : " << std::endl;
+    norminou.beRepaired(10);
+    norminator.beRepaired(10);
+    norminou.takeDamage(10);
+    norminator.takeDamage(10);
+    std::cout << "While the function showStatus is inherited from ClapTrap, they should reflect the difference in status: " << std::endl;
+	norminou.showStatus();
+	norminator.showStatus();
 
+    std::cout << "Destructors (should be ScavTrap then ClapTrap) : " << std::endl;
 	return (0);
 }
